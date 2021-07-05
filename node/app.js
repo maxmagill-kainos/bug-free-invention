3d8e9a38509f258bdff935f0b10691beeef662d3
Max Magill <max.magill@kainos.com>
2021-07-05 15:28:45 +0100

    inital node commit

---
+++ node/app.js
@@ -0,0 +1,11 @@
+const express = require('express') 
+const app = express() 
+ 
+app.get('/', function (req, res) { 
+   res.send('<h1>First message from Express</h1>') 
+   console.log('Request processed'); 
+}); 
+ 
+app.listen(7999, function() { 
+   console.log('Express started') 
+});
