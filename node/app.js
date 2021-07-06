c37cb67873575fca15e932822280cc43dfddce94
Josh Craig <josh.craig@kainos.com>
2021-07-06 10:00:36 +0100

    routes and query for US001

--- node/app.js
+++ node/app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express') 
 const app = express() 
  
@@ -5,6 +6,10 @@ app.get('/', function (req, res) {
    res.send('<h1>First message from Express</h1>') 
    console.log('Request processed'); 
 }); 
+
+app.get('/job-roles', async(req, res) => {
+   res.render('list-job-roles', {jobRoles: await jobData.getJobRoles()});
+})
  
 app.listen(7999, function() { 
    console.log('Express started') 
