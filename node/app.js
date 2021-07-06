b80887eb5ac3a47f2c19e6913756404f68508dc9
paluch05 <annapaluch05@gmail.com>
2021-07-06 11:27:23 +0200

    add another connection do js

--- node/app.js
+++ node/app.js
@@ -20,4 +20,10 @@ app.listen(6555, function() {
    console.log('Express started') 
 });
 
-
+app.get('/fromc', async function (req, res) { 
+   console.log('Request processed'); 
+   const response = await fetch('http://localhost:8080/api/demo/hello-fromc',{method:'GET',headers:{}})
+   const data = await response.text();
+   console.log(response)
+   res.send('<p>'+data+'</p>');
+}); 
