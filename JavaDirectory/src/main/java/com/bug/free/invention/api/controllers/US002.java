1b4bc70d41ffcc3f8ef64f0bf97ffa33e797e792
harrison-duffield <86047213+harrison-duffield@users.noreply.github.com>
2021-07-06 14:41:23 +0100

    Job Spec return
    Assuming DB works(query runs, but currently empty set) :
    Will return the job spec associated with the ID its called with, would actually just like to combine this with one method for the whole object ( this can be raised later on)

--- JavaDirectory/src/main/java/com/bug/free/invention/api/controllers/US002.java
+++ JavaDirectory/src/main/java/com/bug/free/invention/api/controllers/US002.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Connection;
@@ -28,7 +29,7 @@ import java.util.List;
             return returnList;
         };
 
-        @GetMapping("/api/test")
+        @GetMapping("/jobRoles")
         public List<Job> getJobRoles(){
             try{
 
@@ -51,16 +52,17 @@ import java.util.List;
                 return null;
             }
         };
-
-        public String getJobSpecLink(Integer JobID){
+        @GetMapping("/jobSpec")
+        public String getJobSpecLink(@RequestParam Integer JobID){
             String dbQuery = "SELECT 'Job_Spec' FROM `Job` WHERE 'Job_ID' = ?;";
             try(Connection DatabaseConnection = DBConfig.getConnection()){
                 PreparedStatement FindJobSpecById = DatabaseConnection.prepareStatement(dbQuery);
                 FindJobSpecById.setInt(1,JobID);
                 ResultSet results = FindJobSpecById.executeQuery();
-
-                DatabaseConnection.commit();
-                return results.getString("Job_Spec");
+                while(results.next()){
+                    return results.getString("Job_Spec");
+                }
+                return "Incorrect Parameter";
             }
             catch (Exception e){
                 e.printStackTrace();
