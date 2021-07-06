1b600024bf3ad0104addd37eec76e16107abe25d
harrison-duffield <86047213+harrison-duffield@users.noreply.github.com>
2021-07-06 10:17:00 +0100

    Get Request Working Between Java and Node

--- node/app.js
+++ node/app.js
@@ -1,11 +1,23 @@
+const { response } = require('express');
 const express = require('express') 
+const fetch = require('node-fetch')
 const app = express() 
- 
-app.get('/', function (req, res) { 
-   res.send('<h1>First message from Express</h1>') 
+
+
+
+
+app.get('/', async function (req, res) { 
    console.log('Request processed'); 
+   const response = await fetch('http://localhost:8080/api/demo/hello-world',{method:'GET',headers:{}})
+   const data = await response.text();
+   console.log(response)
+   res.send('<p>'+data+'</p>');
 }); 
- 
-app.listen(7999, function() { 
+
+
+
+app.listen(6555, function() { 
    console.log('Express started') 
 });
+
+
