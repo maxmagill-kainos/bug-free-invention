32a1dd5fbeb8c4154afe4bea96929bc3a5b3364c
James <james.hanna@kainos.com>
2021-07-13 16:13:31 +0100

    Fixed changes and added editing feature for Job Table

--- JavaDirectory/src/main/java/com/bug/free/invention/api/controllers/JobController.java
+++ JavaDirectory/src/main/java/com/bug/free/invention/api/controllers/JobController.java
@@ -16,20 +16,7 @@ import java.util.stream.StreamSupport;
 @RestController
     @RequestMapping("/api/jobs")
     public class JobController {
-
-    @Autowired
-    private JobService JobService;
-
-    @GetMapping("/JSONDemo")
-    public List<Employee> demoJson() {
-        List<Employee> returnList = new ArrayList<Employee>();
-        returnList.add(new Employee(1, "name", "difjidsfj"));
-        returnList.add(new Employee(2, "ijisdfjdsif", "dsuifjhduisfhui"));
-        return returnList;
-    }
-
-    ;
-
+        
     @PostMapping(value = "/submitJobSpec", consumes = "application/json", produces = "application/json")
     public String JobSpecSubmit(@RequestBody ObjectNode objectNode) {
         try {
