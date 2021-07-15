27b083b5ecb0abaa902ead6e0cfc56fb7e6acd41
harrison-duffield <86047213+harrison-duffield@users.noreply.github.com>
2021-07-15 12:38:23 +0100

    Update DBConfig.java

--- JavaDirectory/src/main/java/com/bug/free/invention/api/controllers/DBConfig.java
+++ JavaDirectory/src/main/java/com/bug/free/invention/api/controllers/DBConfig.java
@@ -1,5 +1,7 @@
 package com.bug.free.invention.api.controllers;
 
+import org.apache.catalina.User;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,7 +10,7 @@ public class DBConfig {
     private  static String Username = "";
     private static String Password = "" ;
     private static String host = "";
-    private static String DatabaseID = "";
+    private static String DatabaseID = "teamCteam_Josh";
     public static String url = "jdbc:mysql://"+ host + "/"+DatabaseID+"?useSSL=false";
     public DBConfig(){
 
@@ -24,14 +26,18 @@ public class DBConfig {
         }
     }
     public static boolean TestMode(){
+
         DatabaseID = "teamC_Josh";
-        url = "jdbc:h2:mem:teamC_josh;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=MYSQL;DATABASE_TO_UPPER=false";
+        url = "jdbc:h2:mem:teamC_josh;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=MYSQL;DATABASE_TO_UPPER=false;IGNORECASE=TRUE";
         Username = "sa";
         Password ="";
+        System.out.println("in Test Mode : "+Username);
         return true;
     }
     public static boolean ProdMode(){
-        DatabaseID = "teamC_Josh";
+        DatabaseID = "teamCteam_Josh";
+        Username = "";
+        Password = "";
         url = "jdbc:mysql://"+ host + "/"+DatabaseID+"?useSSL=false";
         return true;
     };
