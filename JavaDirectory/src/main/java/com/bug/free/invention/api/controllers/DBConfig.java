fe66db020458a9bff66d29ef1db3b74cee0d4809
craig-j29 <86012530+craig-j29@users.noreply.github.com>
2021-07-16 15:28:06 +0100

    removed db details

--- JavaDirectory/src/main/java/com/bug/free/invention/api/controllers/DBConfig.java
+++ JavaDirectory/src/main/java/com/bug/free/invention/api/controllers/DBConfig.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DBConfig {
 
-    private static final String Username = "tCadmin";
-    private static final String Password = "teamCpassword" ;
-    private static final String host = "academy2020.cpc8rvmbbd9k.eu-west-2.rds.amazonaws.com";
+    private static final String Username = "";
+    private static final String Password = "" ;
+    private static final String host = "";
 
     public static Connection getConnection() throws SQLException {
         try{
