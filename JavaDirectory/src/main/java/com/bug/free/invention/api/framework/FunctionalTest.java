1e77526b775381bb1555fb345911424168b0938e
craig-j29 <86012530+craig-j29@users.noreply.github.com>
2021-07-19 10:58:51 +0100

    spins up spring if only running tests

--- JavaDirectory/src/main/java/com/bug/free/invention/api/framework/FunctionalTest.java
+++ JavaDirectory/src/main/java/com/bug/free/invention/api/framework/FunctionalTest.java
@@ -1,11 +1,13 @@
 package com.bug.free.invention.api.framework;
 
+import com.bug.free.invention.api.ApiApplication;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.SpringApplication;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,6 +20,11 @@ public class FunctionalTest {
 
     @BeforeAll
     public static void setUp() {
+        try {
+            SpringApplication.run(ApiApplication.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         WebDriverManager.chromedriver().browserVersion(browserVersion).setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
